dca4e6d3e87bf07ef4fe5e85351b51f592e924e7
evelyneggres <115740297+evelyneggres@users.noreply.github.com>
2023-05-30 15:00:41 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper",{
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
